61ad73251ccd478bfee39ed17928f09475ed3549
vsaravan21 <166088632+vsaravan21@users.noreply.github.com>
2025-05-31 22:29:09 -0700

    Made all the puzzle pieces disappearing on click

--- Assets/Scripts/addItem.cs
+++ Assets/Scripts/addItem.cs
@@ -2,13 +2,24 @@ using UnityEngine;
 
 public class AddItemButton : MonoBehaviour
 {
-    public string itemName; 
+    public string itemName;
+
+    void Start()
+    {
+        // If the item was collected during this session, hide it
+        if (SessionCollectedItems.collectedItems.Contains(itemName))
+        {
+            gameObject.SetActive(false);
+        }
+    }
 
     public void AddItemToInventory()
     {
         if (InventoryManager.Instance != null)
         {
             InventoryManager.Instance.AddItem(itemName);
+            SessionCollectedItems.collectedItems.Add(itemName); // Mark as collected this session
+            gameObject.SetActive(false);
         }
         else
         {
