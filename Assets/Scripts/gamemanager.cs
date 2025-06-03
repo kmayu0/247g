0c8d8d10b93c0bb0f74993798a883150dbe96814
kmayu0 <kmayu@stanford.edu>
2025-06-03 12:26:18 -0700

    code working to unlock door yay

--- Assets/Scripts/gamemanager.cs
+++ Assets/Scripts/gamemanager.cs
@@ -11,18 +11,19 @@ public class Main : MonoBehaviour
     public bool win;
     public GameObject title;
 public TMPro.TextMeshProUGUI scoreboard;
-public GameObject winpanel;
 
     void Start()
     {
         score = 0;
         title.SetActive(false);
-                        winpanel.SetActive(false);
-
         gameOver = false;
         win = false;
         scoreboard.text = "0";
-
+        GameObject basket = GameObject.Find("basket");
+    if (basket == null)
+        Debug.LogError("Basket NOT found!");
+    else
+        Debug.Log("Basket found.");
     }
 
     void Update()
@@ -36,8 +37,6 @@ public GameObject winpanel;
         {
             title.SetActive(false); // Deactivate the title (hide the game over screen)
             if (score == 30) {
-                        SceneManager.LoadScene("endscene");
-                // winpanel.SetActive(true);
                 win = true;
             }
 }
