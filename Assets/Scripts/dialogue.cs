0897dca1b209b256cbe3b25f0ed7724569151597
kmayu0 <kmayu@stanford.edu>
2025-06-03 11:56:01 -0700

    dialogue

---
+++ Assets/Scripts/dialogue.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem; // For the new Input System
+
+public class DialogueImageSwitcher : MonoBehaviour
+{
+    public Image dialogueImage;
+    public Sprite[] dialogueSprites;
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        if (dialogueSprites.Length > 0)
+        {
+            dialogueImage.sprite = dialogueSprites[0];
+        }
+    }
+
+    void Update()
+    {
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            AdvanceDialogue();
+        }
+    }
+
+    void AdvanceDialogue()
+    {
+        currentIndex++;
+        if (currentIndex < dialogueSprites.Length)
+        {
+            dialogueImage.sprite = dialogueSprites[currentIndex];
+        }
+        else
+        {
+            Debug.Log("End of dialogue.");
+        }
+    }
+}
